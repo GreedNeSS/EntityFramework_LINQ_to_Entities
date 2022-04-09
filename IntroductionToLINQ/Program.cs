69f7cf002a53ced0f1c85e9239bdd6d1c762ab0a
GreedNeSS <ruslan199126@gmail.com>
2022-04-09 16:07:05 +0300

    Добавьте файлы проекта.

---
+++ IntroductionToLINQ/Program.cs
@@ -0,0 +1,18 @@
+﻿using IntroductionToLINQ;
+using IntroductionToLINQ.Models;
+
+Console.WriteLine("***** Introduction to LINQ to Entities *****");
+
+List<Company> companies = new List<Company>();
+companies.Add(new Company { Title = "Microsoft" });
+companies.Add(new Company { Title = "Apple" });
+
+List<User> users = new List<User>();
+users.Add(new User { Name = "GreedNeSS", CompanyId = 1 });
+users.Add(new User { Name = "Marcus", CompanyId = 1 });
+users.Add(new User { Name = "Henry", CompanyId = 2 });
+users.Add(new User { Name = "Alice", CompanyId = 2 });
+
+Utils.CterateEmployeeDB(users, companies);
+Utils.ShowCompanyEmployees_LINQOperators(1);
+Utils.ShowCompanyEmployees_LINQMethods(2);
---
+++ IntroductionToLINQ/Utils.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntroductionToLINQ.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntroductionToLINQ
+{
+    public static class Utils
+    {
+        public static void CterateEmployeeDB(List<User> users, List<Company> companies)
+        {
+            Console.WriteLine("\n=> CterateEmployeeDB()\n");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.Users.AddRange(users);
+                db.Companies.AddRange(companies);
+                db.SaveChanges();
+            }
+        }
+
+        public static void ShowCompanyEmployees_LINQOperators(int companyId)
+        {
+            Console.WriteLine($"\n=> ShowCompanyEmployees_LINQOperators({companyId})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = from user in db.Users.Include(u => u.Company)
+                            where user.CompanyId == companyId
+                            select user;
+
+                if (users != null)
+                {
+                    Console.WriteLine($"\nCompanyName: {users.FirstOrDefault()?.Company?.Title}");
+
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine(user);
+                    }
+                }            
+            }
+        }
+
+        public static void ShowCompanyEmployees_LINQMethods(int companyId)
+        {
+            Console.WriteLine($"\n=> ShowCompanyEmployees_LINQMethods({companyId})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.Include(u => u.Company).Where(u => u.CompanyId == companyId);
+
+                if (users != null)
+                {
+                    Console.WriteLine($"\nCompanyName: {users.FirstOrDefault()?.Company?.Title}");
+
+                    foreach (var user in users)
+                    {
+                        Console.WriteLine(user);
+                    }
+                }            
+            }
+        }
+    }
+}
