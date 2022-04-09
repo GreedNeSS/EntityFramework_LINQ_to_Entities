086ab2a1c18be1a48bbfa0111117befcda41235f
GreedNeSS <ruslan199126@gmail.com>
2022-04-09 19:01:02 +0300

    Sorted and Projection from Database

---
+++ SortingAndProjection/Program.cs
@@ -0,0 +1,24 @@
+﻿using SortingAndProjection;
+using SortingAndProjection.Models;
+
+Console.WriteLine("***** Sorted and Projection from Database *****");
+
+List<Company> companies = new List<Company>();
+companies.Add(new Company { Title = "Microsoft" });
+companies.Add(new Company { Title = "Apple" });
+
+List<User> users = new List<User>();
+users.Add(new User { Name = "GreedNeSS", Age = 30, CompanyId = 1 });
+users.Add(new User { Name = "Marcus", Age = 45, CompanyId = 1 });
+users.Add(new User { Name = "Marc", Age = 32, CompanyId = 2 });
+users.Add(new User { Name = "Marc", Age = 22, CompanyId = 1 });
+users.Add(new User { Name = "Henry", Age = 40, CompanyId = 2 });
+users.Add(new User { Name = "Henry", Age = 20, CompanyId = 1 });
+users.Add(new User { Name = "Henry", Age = 36, CompanyId = 2 });
+users.Add(new User { Name = "Alice", Age = 27, CompanyId = 1 });
+
+Utils.CterateEmployeeDB(users, companies);
+List<UserModel> userModels = Utils.GetAllUserModels(Utils.GetAllUser());
+Utils.ShowUserModels(userModels);
+Utils.ShowUserModels(Utils.OrderedUserModelsByName(userModels));
+Utils.ShowUserModels(Utils.OrderedUserModelsByNameAndAge(userModels));
---
+++ SortingAndProjection/Utils.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SortingAndProjection.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SortingAndProjection
+{
+    public static class Utils
+    {
+        public static void CterateEmployeeDB(List<User> users, List<Company> companies)
+        {
+            Console.WriteLine("\n=> CterateEmployeeDB()\n");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.Users.AddRange(users);
+                db.Companies.AddRange(companies);
+                db.SaveChanges();
+            }
+        }
+
+        public static void ShowUserModels(List<UserModel> users)
+        {
+            Console.WriteLine("\n=> ShowUsers():");
+
+            foreach (var user in users)
+            {
+                ShowUserModel(user);
+            }
+        }
+
+        public static void ShowUserModel(UserModel user)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(user);
+            Console.ResetColor();
+        }
+
+        public static List<UserModel> GetAllUserModels(List<User> users)
+        {
+            Console.WriteLine("\n=> GetAllUserModels()");
+
+            var userModels = users
+                .Select(u => new UserModel
+                {
+                    Name = u.Name,
+                    Age = u.Age,
+                    CompanyTitle = u.Company!.Title
+                })
+                .ToList();
+
+            return userModels;
+        }
+
+        public static List<User> GetAllUser()
+        {
+            Console.WriteLine($"\n=> GetAllUser()");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.Include(u => u.Company).ToList();
+
+                return users;           
+            }
+        }
+
+        public static List<UserModel> OrderedUserModelsByName(List<UserModel> userModels)
+        {
+            Console.WriteLine($"\n=> OrderedUserModelsByName()");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = from userModel in userModels
+                            orderby userModel.Name
+                            select userModel;
+
+                return users.ToList();           
+            }
+        }
+
+        public static List<UserModel> OrderedUserModelsByNameAndAge(List<UserModel> userModels)
+        {
+            Console.WriteLine($"\n=> OrderedUserModelsByNameAndAge()");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                //var users = from userModel in userModels
+                //            orderby userModel.Name, userModel.Age
+                //            select userModel;
+
+                var users = userModels
+                    .OrderBy(u => u.Name)
+                    .ThenBy(u => u.Age);
+
+                return users.ToList();           
+            }
+        }
+    }
+}
