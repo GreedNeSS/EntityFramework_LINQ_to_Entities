f1b86c636b949acecd5319dc72cb040642fce9b1
GreedNeSS <ruslan199126@gmail.com>
2022-04-09 17:27:49 +0300

    Fixed bugs.

--- IntroductionToLINQ/Models/Company.cs
+++ IntroductionToLINQ/Models/Company.cs
@@ -10,6 +10,6 @@ namespace IntroductionToLINQ.Models
     {
         public int Id { get; set; }
         public string? Title{ get; set; }
-        public List<User> Users { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
     }
 }
