94a8405375dacf603cc40485444b7b87d6461d15
GreedNeSS <ruslan199126@gmail.com>
2022-04-09 17:27:11 +0300

    Added filtering example.

---
+++ Filtering/Program.cs
@@ -0,0 +1,26 @@
+﻿using Filtering;
+using Filtering.Models;
+
+Console.WriteLine("***** Filtering *****");
+
+List<Company> companies = new List<Company>();
+companies.Add(new Company { Title = "Microsoft" });
+companies.Add(new Company { Title = "Apple" });
+
+List<User> users = new List<User>();
+users.Add(new User { Name = "GreedNeSS", Age = 30, CompanyId = 1 });
+users.Add(new User { Name = "Marcus", Age = 45, CompanyId = 1 });
+users.Add(new User { Name = "Marc", Age = 32, CompanyId = 2 });
+users.Add(new User { Name = "Henry", Age = 20, CompanyId = 2 });
+users.Add(new User { Name = "Alice", Age = 27, CompanyId = 2 });
+
+Utils.CterateEmployeeDB(users, companies);
+Utils.ShowUsers(Utils.GetCompanyEmployees("Microsoft"));
+Utils.ShowUsers(Utils.GetUsersByName("%Marc%"));
+Utils.ShowUsers(Utils.GetUsersByAge("3_"));
+User? user3 = Utils.GetUser(3);
+
+if (user3 != null)
+{
+    Utils.ShowUser(user3);
+}
---
+++ Filtering/Utils.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Filtering.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filtering
+{
+    public static class Utils
+    {
+        public static void CterateEmployeeDB(List<User> users, List<Company> companies)
+        {
+            Console.WriteLine("\n=> CterateEmployeeDB()\n");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+                db.Users.AddRange(users);
+                db.Companies.AddRange(companies);
+                db.SaveChanges();
+            }
+        }
+
+        public static void ShowUsers(List<User> users)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n=> ShowUsers():");
+
+            foreach (var user in users)
+            {
+                Console.WriteLine(user);
+            }
+
+            Console.ResetColor();
+        }
+
+        public static void ShowUser(User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\n=> ShowUser():");
+            Console.WriteLine(user);
+            Console.ResetColor();
+        }
+
+        public static List<User> GetCompanyEmployees(string companyTitle)
+        {
+            Console.WriteLine($"\n=> GetCompanyEmployees({companyTitle})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.Include(u => u.Company)
+                    .Where(u => u.Company!.Title == companyTitle)
+                    .ToList();
+
+                return users;           
+            }
+        }
+
+        public static List<User> GetUsersByName(string namePattern)
+        {
+            Console.WriteLine($"\n=> GetUsersByName({namePattern})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = db.Users.Include(u => u.Company)
+                    .Where(u => EF.Functions.Like(u.Name!, namePattern))
+                    .ToList();
+
+                return users;           
+            }
+        }
+
+        public static List<User> GetUsersByAge(string agePattern)
+        {
+            Console.WriteLine($"\n=> GetUsersByAge({agePattern})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var users = from user in db.Users.Include(u => u.Company)
+                            where EF.Functions.Like(user.Age.ToString(), agePattern)
+                            select user;
+
+                return users.ToList();           
+            }
+        }
+
+        public static User? GetUser(int userId)
+        {
+            Console.WriteLine($"\n=> GetUser({userId})");
+
+            using (ApplicationContext db = new ApplicationContext())
+            {
+                var user = db.Users.Include(u => u.Company)
+                    .Where(u => u.Id == userId).FirstOrDefault();
+
+                return user;
+            }
+        }
+    }
+}
