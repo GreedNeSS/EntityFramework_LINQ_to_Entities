f6c05b3651d2ff0205a67a971fef8fb938e1d5eb
GreedNeSS <ruslan199126@gmail.com>
2022-04-10 14:53:35 +0300

    Added ChangeTracker and AsNoTracking example.

---
+++ ChangeTrackerAndAsNoTracking/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChangeTrackerAndAsNoTracking
+{
+    internal class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("DataSource=Users.db");
+        }
+    }
+}
---
+++ ChangeTrackerAndAsNoTracking/Program.cs
@@ -0,0 +1,72 @@
+﻿using ChangeTrackerAndAsNoTracking;
+using Microsoft.EntityFrameworkCore;
+
+Console.WriteLine("***** ChangeTracker And AsNoTracking *****");
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.Database.EnsureDeleted();
+    db.Database.EnsureCreated();
+
+    User greedNess = new User { Name = "GreedNeSS", Age = 30 };
+    User marcus = new User { Name = "Marcus", Age = 45 };
+    User tom = new User { Name = "Tom", Age = 22 };
+    User terry = new User { Name = "Terry", Age = 36 };
+
+    db.Users.AddRange(greedNess, marcus, tom, terry);
+    db.SaveChanges();
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    User? user = db.Users.AsNoTracking().FirstOrDefault();
+
+    if (user != null)
+    {
+        user.Age = 12;
+        db.SaveChanges();
+    }
+
+    Console.WriteLine("\n=>db.Users.AsNoTracking().FirstOrDefault()\n");
+    var users = db.Users.ToList();
+
+    foreach (var u in users)
+    {
+        Console.WriteLine(u);
+    }
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    db.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    User? user = db.Users.FirstOrDefault();
+
+    if (user != null)
+    {
+        user.Age = 12;
+        db.SaveChanges();
+    }
+
+    Console.WriteLine("\n=> db.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking\n");
+    var users = db.Users.ToList();
+
+    foreach (var u in users)
+    {
+        Console.WriteLine(u);
+    }
+}
+
+using (ApplicationContext db = new ApplicationContext())
+{
+    Console.WriteLine("\n=> \n");
+    var users = db.Users.ToList();
+
+    foreach (var u in users)
+    {
+        Console.WriteLine(u);
+    }
+
+    int count = db.ChangeTracker.Entries().Count();
+
+    Console.WriteLine($"\n=> db.ChangeTracker.Entries().Count() = {count}");
+}
